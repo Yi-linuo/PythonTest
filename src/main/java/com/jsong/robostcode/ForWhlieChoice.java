e1615c8a5c0fa7de738f979bad21b196e7da2eed
qianjinfu@gmail.com <changsong2008>
2024-12-08 19:49:04 +0800

    如何处写出健壮的代码

--- src/main/java/com/jsong/robostcode/ForWhlieChoice.java
+++ src/main/java/com/jsong/robostcode/ForWhlieChoice.java
@@ -9,7 +9,8 @@ import java.util.List;
 /**
  * 如何处写出健壮的代码
  *
- * 参考： Effective Java 第57条 :最小花局部变量的作用域
+ * 参考： Effective Java 第57条 :最小化局部变量的作用域
+ *                      第58条 :for-each循环优先于传统的for循环
  *
  */
 @Slf4j
@@ -24,8 +25,18 @@ public class ForWhlieChoice {
         list.add("a");
         list.add("b");
 
+        List<String> lists = new ArrayList<String>(10);
+        lists.add("a");
+
         // Interface-based iteration
-        for (Iterator<String> it = list.iterator(); it.hasNext(); ) {
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()) {
+            String str = it.next();
+            log.info(str);
+        }
+
+        Iterator<String> it2 = lists.iterator();
+        while (it2.hasNext()) {
             String str = it.next();
             log.info(str);
         }
